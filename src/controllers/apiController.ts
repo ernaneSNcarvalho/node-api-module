2fb1e9443d500a308ccf0f5a032f56129ef8f024
ernane <ernanecarvalho89@gmail.com>
2022-07-18 07:09:53 -0300

    trabalhando com imagens

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -56,3 +56,11 @@ export const deletarFrase = async (req: Request, res: Response) => {
     });
     res.json({});
 }
+
+export const uploadFile = async (req: Request, res: Response) => {
+    const files = req.files as {[fieldname: string]: Express.Multer.File[]}
+    console.log("AVATAR", files.avatar);
+    console.log("GALLERY", files.gallery);
+
+    res.json({})
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,11 @@
 import {Router} from 'express';
 import { json } from 'sequelize/types';
 import * as ApiController from '../controllers/apiController';
+import multer from 'multer';
+
+const upload = multer({
+    dest: './tmp'
+});
 
 const router = Router();
 
@@ -9,5 +14,12 @@ router.get('/random', ApiController.random);
 router.get('/nome/:nome', ApiController.nome);
 
 router.post('/frases', ApiController.criarFrases);
-
+router.get('/frases', ApiController.pegarFrases);
+router.get('/frase/:id', ApiController.pegarFrase);
+router.put('/frase/:id', ApiController.atualizarFrase);
+router.delete('/frase/:id', ApiController.deletarFrase)
+router.post('/upload', upload.fields([
+    {name: 'avatar', maxCount: 1}, 
+    {name: 'gallery', maxCount: 3}
+]), ApiController.uploadFile);
 export default router;
